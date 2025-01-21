b2a45204eac2cdc7bd5e27712e3bc468407bccff
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-20 21:55:55 -0600

    Agregar comentarios explicativos en main.py para mejorar la claridad del código

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI()     # Instancia de la clase FastAPI
 
-app.title = "Mi primera aplicacion con FastAPI"
-app.version = "0.0.1"
+app.title = "Mi primera aplicacion con FastAPI" # Titulo de la API
+app.version = "0.0.1" # Version de la API
 
-@app.get("/", tags=["Home"])
-def home():
-    return {"Hola Mundo" : "desde FastAPI por neftali"}
+@app.get("/", tags=["Home"])  # Decorador para definir la ruta de la API
+def home():    # Funcion que se ejecutara al ingresar a la ruta
+    return {"Hola Mundo" : "desde FastAPI por neftali"} # Respuesta de la API
 
