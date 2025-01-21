0c78d67e3b59766b794c9c89bf446c95c33fa1ec
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-20 21:53:55 -0600

    Agregar título y versión a la aplicación FastAPI, y añadir etiqueta a la ruta principal

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+app.title = "Mi primera aplicacion con FastAPI"
+app.version = "0.0.1"
 
-@app.get("/")
+@app.get("/", tags=["Home"])
 def home():
     return {"Hola Mundo" : "desde FastAPI por neftali"}
+
